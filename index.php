511941592e160e7340bd1f75830b12d36c5d4d56
yuzucha01 <78517616+yuzucha01@users.noreply.github.com>
2021-04-04 17:21:53 +0900

    Update index.php

--- index.php
+++ index.php
@@ -30,6 +30,26 @@
 <p>¥5,000</p>
 </div>
 
+<div id="order_name">
+<p>朝日タワー</p>
+<input type="hidden" name="name" value="朝日タワー">
+<img src="./img/02.jpg" alt="">
+<select name="eat1">
+<option value="0">0</option>
+<option value="1">1</option>
+<option value="2">2</option>
+<option value="3">3</option>
+<option value="4">4</option>
+<option value="5">5</option>
+<option value="6">6</option>
+<option value="7">7</option>
+<option value="8">8</option>
+<option value="9">9</option>
+<option value="10">10</option>
+</select>
+<p>¥4,000</p>
+</div>
+
 <p><input type="submit" value="合計を確認する"></p>
 
 </form>
